7d88836ca7651e88fa23145fb3224dfe4ba50ddb
GitHub Action <action@github.com>
2020-05-18 00:02:53 +0000

    Updated Formula

--- Formula/legume.rb
+++ Formula/legume.rb
@@ -3,9 +3,9 @@ require "language/node"
 class Legume < Formula
   desc "Professor Copperfield's Miracle Legumes will help you do anything"
   homepage "https://github.com/trulyronak/legume"
-  url "https://registry.npmjs.org/legume/-/legume-1.5.0.tgz"
-  version "1.5.0"
-  sha256 "0d7d15340c4a70bc2f41ecd39f4bd2ecd2613ad8b10c3730ef62c68ba65577d7"
+  url "https://registry.npmjs.org/legume/-/legume-1.6.0.tgz"
+  version "1.6.0"
+  sha256 "f3e0c923ab6145a6bbc70802c0865b745e4af9dd32bea66637dae339b38b6179"
 
   depends_on "node"
 
